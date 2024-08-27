62ac0982c7f137cf687921bfc189850575e04aa0
ifraimka <mr.ifraim@yandex.ru>
2024-08-27 10:37:15 +0300

    Update version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_gradefilter';
-$plugin->release = '0.1.2';
-$plugin->version = 2024082202;
+$plugin->release = '0.1.3';
+$plugin->version = 2024082700;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
